b637f266ac7dd9223de6bd3b379c4a39bc36c2d3
astalkerals <astalker2014@gmail.com>
2026-02-08 20:17:28 -0500

    javascript

--- csce242/assignments/05-javascript/script.js
+++ csce242/assignments/05-javascript/script.js
@@ -13,7 +13,12 @@ const addBorder = () => {
     document.getElementById("rat").classList.toggle("frame");
 }
 
+const dateMessage = () => {
+    document.getElementById("text").innerHTML = "You picked the date: " + document.getElementById("date-picker").value;
+}
+
 window.onload = () => {
     document.getElementById("geometry").onclick = makeTriangle;
     document.getElementById("rat").onclick = addBorder;
+    document.getElementById("date-picker").onchange = dateMessage;
 };
